33e046e2b23cb82e807717b47f38b75c0e25fb5b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-22 13:43:21 -0300

    Feito no dia 22/05, Bootstrap em React

---
+++ react/revBoot/src/App.jsx
@@ -0,0 +1,15 @@
+import NavProjeto from './componentes/NavProjeto';
+import CardProjeto from './componentes/CardProjeto';
+import CarouselProjeto from './componentes/CarouselProjeto';
+
+function App(){
+  return(
+    <div>
+      <NavProjeto/> <br />
+      <CardProjeto/><br />
+      <CarouselProjeto/>
+    </div>
+  )
+}
+
+export default App

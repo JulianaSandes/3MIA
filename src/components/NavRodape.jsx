3936b709b081f1300bf711e9c5e806a4562e8866
Juliana Sandes <84139776+JulianaSandes@users.noreply.github.com>
2023-05-30 18:21:20 -0300

    Add files via upload

--- src/components/NavRodape.jsx
+++ src/components/NavRodape.jsx
@@ -9,7 +9,6 @@ function NavRodape() {
       <Container>
       <Navbar.Brand href="/">Clique e volte ao topo</Navbar.Brand>
         <Nav className="ms-auto">
-        <h1 class="text-rodape">Juliana Barbosa Sandes-3°Mia</h1>
           <p class="text-rodape">© 2023 Copyright: Juliana Sandes</p>
         </Nav>
       </Container>
--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,7 +1,9 @@
 import NavProjeto from "../components/NavProjeto";
 import Carrossel from "../components/Carrossel";
 import Cards from "../components/Cards";
+import Forms from "../components/Forms"
 import Cabecalho from "../components/Cabecalho";
+import { BsInstagram, BsLinkedin, BsTwitch, BsTwitter } from "react-icons/bs";
 //import "../App.css";
 
 function Contato() {
@@ -13,11 +15,29 @@ function Contato() {
         <div className="sobre">
         <h1>Entre em contato com a gente</h1>
         </div>
-        <div>
-          <p>
-          colocar icons           
-          </p>
-        </div>
+          <div>
+            <Forms/>
+          </div>
+          <h2 className="subtitulo">
+          Venha acompanhar a gente nas redes         
+          </h2>
+          <div className="flex">
+          <div className="icons"> 
+          <BsInstagram/>
+          </div>
+          <div className="icons">
+          <BsLinkedin/>
+          </div>
+          <div className="icons">
+          <BsTwitch/>
+          </div>
+          <div className="icons">
+          <BsTwitter/>
+          </div>
+          </div>
+          
+          
+          
     </div>
     </div>
   );
